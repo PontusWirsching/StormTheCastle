ab8e4a2ba53816555474082fcf8e5514cd0a03e8
PontusWirsching <flash13records@gmail.com>
2015-08-25 22:20:07 +0200

    Uploading default project

---
+++ android/src/com/core/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.core.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.core.StormTheCastle;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new StormTheCastle(), config);
+	}
+}
---
+++ desktop/src/com/core/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.core.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.core.StormTheCastle;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new StormTheCastle(), config);
+	}
+}
