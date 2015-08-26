97d2eba66183a2dd502443cdd322801df618404b
PontusWirsching <flash13records@gmail.com>
2015-08-26 23:48:44 +0200

    Removed stupid comment!
    Signed-off-by: PontusWirsching <flash13records@gmail.com>

--- core/src/com/core/StormTheCastle.java
+++ core/src/com/core/StormTheCastle.java
@@ -4,8 +4,6 @@ import com.badlogic.gdx.ApplicationListener;
 
 public class StormTheCastle implements ApplicationListener {
 
-	// This is me commenting from the github website!
-
 	@Override
 	public void create() {
 		
