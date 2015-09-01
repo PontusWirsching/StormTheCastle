bc5152bcd4fc6aa469d402f5f82a10fd53ec9c87
Mike McEnery <AlysiumX@gmail.com>
2015-09-01 12:27:53 -0400

    Started working on getting sprites in the game.

---
+++ core/src/com/core/game/Entity.java
@@ -0,0 +1,20 @@
+package com.core.game;
+
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+/**
+ * An entity represents a game object it extends the sprite class.
+ * 
+ * @author Mike McEnery
+ * @since 2015-08-29
+ *
+ */
+public class Entity extends Sprite {
+	public Entity(String entityKey){
+		
+	}
+	
+	public void Draw(SpriteBatch spriteBatch){
+		
+	}
+}
---
+++ core/src/com/core/game/types/EntityKeys.java
@@ -0,0 +1,5 @@
+package com.core.game.types;
+
+public class EntityKeys{
+	public final String GoblinKey = "Goblin";
+}
